2e6608bed8e5c3a0d4364f3e9b070bb51d6ca957
Dave Keck <dave@getshacked.com>
2014-08-27 01:19:03 -0400

    Updated test case

--- chantest/chantest/main.m
+++ chantest/chantest/main.m
@@ -7,6 +7,60 @@
 
 eb_chan_t gChan = NULL;
 
+void *threadDoSend(void *a)
+{
+    eb_chan_op_t send = eb_chan_send(gChan, "halla");
+    eb_chan_op_t *const ops[] = {&send};
+    for (NSUInteger i = 0; i < NTRIALS; i++) {
+        assert(eb_chan_do(ops, (sizeof(ops) / sizeof(*ops))));
+        NSLog(@"SENT");
+    }
+    return NULL;
+}
+
+void *threadTryRecv(void *a)
+{
+    eb_chan_op_t recv = eb_chan_recv(gChan);
+    eb_chan_op_t *const ops[] = {&recv};
+    for (NSUInteger i = 0; i < NTRIALS; i++) {
+        if (eb_chan_try(ops, (sizeof(ops) / sizeof(*ops))) == &recv) {
+            NSLog(@"RECEIVED");
+        } else {
+            NSLog(@"NOT RECEIVED");
+        }
+    }
+    return NULL;
+}
+
+
+
+
+void *threadDoRecv(void *a)
+{
+    eb_chan_op_t recv = eb_chan_recv(gChan);
+    eb_chan_op_t *const ops[] = {&recv};
+    for (NSUInteger i = 0; i < NTRIALS; i++) {
+        assert(eb_chan_do(ops, (sizeof(ops) / sizeof(*ops))));
+        NSLog(@"RECEIVED");
+    }
+    return NULL;
+}
+
+void *threadTrySend(void *a)
+{
+    eb_chan_op_t send = eb_chan_send(gChan, "sup g");
+    eb_chan_op_t *const ops[] = {&send};
+    for (NSUInteger i = 0; i < NTRIALS; i++) {
+        if (eb_chan_do(ops, (sizeof(ops) / sizeof(*ops))) == &send) {
+            NSLog(@"SENT");
+        } else {
+            NSLog(@"NOT SENT");
+            abort();
+        }
+    }
+    return NULL;
+}
+
 void *threadSend(void *a)
 {
     eb_chan_op_t send = eb_chan_send(gChan, "hallo");
@@ -55,6 +109,12 @@ int main(int argc, const char * argv[])
     gChan = eb_chan_alloc(0);
     
     pthread_t thread1, thread2;
+    
+//    pthread_create(&thread1, NULL, threadDoSend, NULL);
+//    pthread_create(&thread2, NULL, threadTryRecv, NULL);
+    
+//    pthread_create(&thread1, NULL, threadDoRecv, NULL);
+//    pthread_create(&thread2, NULL, threadTrySend, NULL);
     
     pthread_create(&thread1, NULL, threadSend, NULL);
     pthread_create(&thread2, NULL, threadRecv, NULL);
