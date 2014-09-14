ac171fdbff5e6a6bee0b55304362a2483557f6e2
Dave Keck <dave@getshacked.com>
2014-09-14 03:59:24 -0400

    nsec changes

--- src/eb_time.h
+++ src/eb_time.h
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 typedef uint64_t eb_nsec; /* Units of nanoseconds */
-#define eb_nsec_zero 0
+#define eb_nsec_zero UINT64_C(0)
 #define eb_nsec_forever UINT64_MAX
 #define eb_nsec_per_sec UINT64_C(1000000000)
 
