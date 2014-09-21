a76936d6efeffce135a2c2e63d9816a4815af94e
Dave Keck <dave@getshacked.com>
2014-09-20 20:13:07 -0400

    Got select7 testcase working

--- src/test.c
+++ src/test.c
@@ -1,43 +1,78 @@
 // DONE
 
-// Test for select: Issue 2075
-// A bug in select corrupts channel queues of failed cases
-// if there are multiple waiters on those channels and the
-// select is the last in the queue. If further waits are made
-// on the channel without draining it first then those waiters
-// will never wake up. In the code below c1 is such a channel.
+// Test select when discarding a value.
 
 #include "testglue.h"
 
+void recv1(eb_chan c) {
+    assert(eb_chan_recv(c, NULL));
+}
+
+void recv2(eb_chan c) {
+    eb_chan_op recv = eb_chan_recv_op(c);
+    assert(eb_chan_do(eb_nsec_forever, &recv) == &recv);
+}
+
+void recv3(eb_chan c) {
+    eb_chan c2 = eb_chan_create(0);
+    eb_chan_op crecv = eb_chan_recv_op(c);
+    eb_chan_op c2recv = eb_chan_recv_op(c2);
+    eb_chan_op *r = eb_chan_do(eb_nsec_forever, &crecv, &c2recv);
+    if (r == &crecv) {
+    } else if (r == &c2recv) {
+    } else {
+        abort();
+    }
+}
+
+typedef void (*RecvFunc)(eb_chan c);
+
+void send1(RecvFunc recv) {
+	eb_chan c = eb_chan_create(0);
+	go( recv(c) );
+    usleep(10);
+    eb_chan_send(c, (void*)1);
+}
+
+void send2(RecvFunc recv) {
+	eb_chan c = eb_chan_create(0);
+	go( recv(c) );
+	usleep(10);
+    
+    eb_chan_op csend = eb_chan_send_op(c, (void*)1);
+    assert(eb_chan_do(eb_nsec_forever, &csend) == &csend);
+}
+
+void send3(RecvFunc recv) {
+	eb_chan c = eb_chan_create(0);
+	go( recv(c) );
+	usleep(10);
+    
+    eb_chan c2 = eb_chan_create(0);
+    
+    eb_chan_op csend = eb_chan_send_op(c, (void*)1);
+    eb_chan_op c2send = eb_chan_send_op(c2, (void*)1);
+    eb_chan_op *r = eb_chan_do(eb_nsec_forever, &csend, &c2send);
+    if (r == &csend) {
+    } else if (r == &c2send) {
+    } else {
+        abort();
+    }
+}
+
 int main() {
-	eb_chan c1 = eb_chan_create(0);
-	eb_chan c2 = eb_chan_create(0);
-	eb_chan c3 = eb_chan_create(0);
-    
-    go( eb_chan_recv(c1, NULL) );
-    
-    go(
-        eb_chan_op c1recv = eb_chan_recv_op(c1);
-        eb_chan_op c2recv = eb_chan_recv_op(c2);
-        eb_chan_op *r = eb_chan_do(eb_nsec_forever, &c1recv, &c2recv);
-        if (r == &c1recv) {
-            abort();
-        } else if (r == &c2recv) {
-            eb_chan_send(c3, (void*)true);
-        } else {
-            abort();
-        }
-        
-        assert(eb_chan_recv(c1, NULL));
-    );
-    
-    go( eb_chan_send(c2, (void*)true) );
-    assert(eb_chan_recv(c3, NULL));
-    
-    eb_chan_send(c1, (void*)true);
-    eb_chan_send(c1, (void*)true);
+	send1(recv1);
+	send2(recv1);
+	send3(recv1);
+	send1(recv2);
+	send2(recv2);
+	send3(recv2);
+	send1(recv3);
+	send2(recv3);
+	send3(recv3);
     return 0;
 }
+
 
 
 
--- test/select7.c
+++ test/select7.c
@@ -1,68 +1,74 @@
-// run
-
-// Copyright 2011 The Go Authors.  All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// DONE
 
 // Test select when discarding a value.
 
-package main
+#include "testglue.h"
 
-import "runtime"
-
-func recv1(c <-chan int) {
-	<-c
+void recv1(eb_chan c) {
+    assert(eb_chan_recv(c, NULL));
 }
 
-func recv2(c <-chan int) {
-	select {
-	case <-c:
-	}
+void recv2(eb_chan c) {
+    eb_chan_op recv = eb_chan_recv_op(c);
+    assert(eb_chan_do(eb_nsec_forever, &recv) == &recv);
 }
 
-func recv3(c <-chan int) {
-	c2 := make(chan int)
-	select {
-	case <-c:
-	case <-c2:
-	}
+void recv3(eb_chan c) {
+    eb_chan c2 = eb_chan_create(0);
+    eb_chan_op crecv = eb_chan_recv_op(c);
+    eb_chan_op c2recv = eb_chan_recv_op(c2);
+    eb_chan_op *r = eb_chan_do(eb_nsec_forever, &crecv, &c2recv);
+    if (r == &crecv) {
+    } else if (r == &c2recv) {
+    } else {
+        abort();
+    }
 }
 
-func send1(recv func(<-chan int)) {
-	c := make(chan int)
-	go recv(c)
-	runtime.Gosched()
-	c <- 1
+typedef void (*RecvFunc)(eb_chan c);
+
+void send1(RecvFunc recv) {
+	eb_chan c = eb_chan_create(0);
+	go( recv(c) );
+    usleep(10);
+    eb_chan_send(c, (void*)1);
 }
 
-func send2(recv func(<-chan int)) {
-	c := make(chan int)
-	go recv(c)
-	runtime.Gosched()
-	select {
-	case c <- 1:
-	}
+void send2(RecvFunc recv) {
+	eb_chan c = eb_chan_create(0);
+	go( recv(c) );
+	usleep(10);
+    
+    eb_chan_op csend = eb_chan_send_op(c, (void*)1);
+    assert(eb_chan_do(eb_nsec_forever, &csend) == &csend);
 }
 
-func send3(recv func(<-chan int)) {
-	c := make(chan int)
-	go recv(c)
-	runtime.Gosched()
-	c2 := make(chan int)
-	select {
-	case c <- 1:
-	case c2 <- 1:
-	}
+void send3(RecvFunc recv) {
+	eb_chan c = eb_chan_create(0);
+	go( recv(c) );
+	usleep(10);
+    
+    eb_chan c2 = eb_chan_create(0);
+    
+    eb_chan_op csend = eb_chan_send_op(c, (void*)1);
+    eb_chan_op c2send = eb_chan_send_op(c2, (void*)1);
+    eb_chan_op *r = eb_chan_do(eb_nsec_forever, &csend, &c2send);
+    if (r == &csend) {
+    } else if (r == &c2send) {
+    } else {
+        abort();
+    }
 }
 
-func main() {
-	send1(recv1)
-	send2(recv1)
-	send3(recv1)
-	send1(recv2)
-	send2(recv2)
-	send3(recv2)
-	send1(recv3)
-	send2(recv3)
-	send3(recv3)
+int main() {
+	send1(recv1);
+	send2(recv1);
+	send3(recv1);
+	send1(recv2);
+	send2(recv2);
+	send3(recv2);
+	send1(recv3);
+	send2(recv3);
+	send3(recv3);
+    return 0;
 }
