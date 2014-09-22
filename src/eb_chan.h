c9e098aa0e43fdfb982832dd0dbbb1eccf7a79b6
Dave Keck <dave@getshacked.com>
2014-09-22 02:12:20 -0400

    Added comments to the header file

--- src/eb_chan.h
+++ src/eb_chan.h
@@ -34,24 +34,31 @@ eb_chan_op *eb_chan_do_list(eb_nsec timeout, eb_chan_op *const ops[], size_t nop
     eb_chan_do_list(timeout, eb_chan_do_ops, (sizeof(eb_chan_do_ops) / sizeof(*eb_chan_do_ops)));   \
 })
 
+/* Returns an initialized send op */
 static inline eb_chan_op eb_chan_op_send(eb_chan c, const void *val) {
     return (eb_chan_op){.chan = c, .send = true, .open = false, .val = val};
 }
 
+/* Returns an initialized recv op */
 static inline eb_chan_op eb_chan_op_recv(eb_chan c) {
     return (eb_chan_op){.chan = c, .send = false, .open = false, .val = NULL};
 }
 
+/* Sends a value on a channel, blocking until it can do so. */
 static inline void eb_chan_send(eb_chan c, const void *val) {
     eb_chan_op op = eb_chan_op_send(c, val);
     assert(eb_chan_do(eb_nsec_forever, &op));
 }
 
+/* Sends a value on a channel and returns true, or returns false if the value couldn't be sent without blocking. */
 static inline bool eb_chan_try_send(eb_chan c, const void *val) {
     eb_chan_op op = eb_chan_op_send(c, val);
     return (eb_chan_do(eb_nsec_zero, &op) != NULL);
 }
 
+/* Receives a value from a channel, blocking until it can do so.
+   Returns true if the value was generated due to a successful send, or false if the channel is closed.
+   'val' is only valid if true is returned. */
 static inline bool eb_chan_recv(eb_chan c, const void **val) {
     eb_chan_op op = eb_chan_op_recv(c);
     assert(eb_chan_do(eb_nsec_forever, &op));
@@ -61,6 +68,9 @@ static inline bool eb_chan_recv(eb_chan c, const void **val) {
     return op.open;
 }
 
+/* Receives a value from a channel and returns true, or returns false if no value could be received without blocking.
+   'open' is set to true if the value was generated due to a successful send, or false if the channel is closed.
+   'val' is only valid if 'open' is true. */
 static inline bool eb_chan_try_recv(eb_chan c, bool *open, const void **val) {
     eb_chan_op op = eb_chan_op_recv(c);
     bool result = (eb_chan_do(eb_nsec_zero, &op) != NULL);
