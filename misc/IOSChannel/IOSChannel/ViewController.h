a4882a9eb2f0db184bc7f322a52388ef8012c34c
Дмитрий Дорофеев <dima@yasp.com>
2015-04-02 23:44:06 +0300

    started iOS project

---
+++ misc/IOSChannel/IOSChannel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSChannel
+//
+//  Created by Дмитрий Дорофеев on 02/04/15.
+//  Copyright (c) 2015 YASP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ misc/IOSChannel/IOSChannel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IOSChannel
+//
+//  Created by Дмитрий Дорофеев on 02/04/15.
+//  Copyright (c) 2015 YASP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
